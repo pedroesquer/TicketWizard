a9082bb54946b9db0833c6faddd7495b1610cdd4
pedroesquer <pedro.morales252815@potros.itson.edu.mx>
2025-02-20 02:10:34 -0700

    Nueva DTO de BoletoEvento

---
+++ src/main/java/itson/usuariosDTOs/NuevoBoletoEventoDTO.java
@@ -0,0 +1,60 @@
+package itson.usuariosDTOs;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author pedro
+ */
+public class NuevoBoletoEventoDTO {
+
+    private String ID;
+    private String asiento; //numero de fila
+    private float precioOriginal; //Precio del boleto desde la boletera
+    private LocalDateTime fechaHora; //Fecha hora del evento
+    private String nombreEv; //Nombre evento
+    private String lugar; //Lugar
+    private String tipo;
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public NuevoBoletoEventoDTO(String ID, String asiento, float precioOriginal, LocalDateTime fechaHora, String nombreEv, String lugar, String tipo) {
+        this.ID = ID; 
+        this.asiento = asiento;
+        this.precioOriginal = precioOriginal;
+        this.fechaHora = fechaHora;
+        this.nombreEv = nombreEv;
+        this.lugar = lugar;
+        this.tipo = tipo;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public String getAsiento() {
+        return asiento;
+    }
+
+    public float getPrecioOriginal() {
+        return precioOriginal;
+    }
+
+    public LocalDateTime getFechaHora() {
+        return fechaHora;
+    }
+
+    public String getNombreEv() {
+        return nombreEv;
+    }
+
+    public String getLugar() {
+        return lugar;
+    }
+    
+    
+    
+    
+}
